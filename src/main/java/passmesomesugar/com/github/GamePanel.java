f72db58d019820fe2b3e89fb4e97da5a469749ed
Sadvakassov Bagzhan <sadvakasov.bagjan@gmail.com>
2021-03-31 16:23:26 +0600

    grid works for frame

--- src/main/java/passmesomesugar/com/github/GamePanel.java
+++ src/main/java/passmesomesugar/com/github/GamePanel.java
@@ -50,6 +50,7 @@ public class GamePanel extends JPanel implements ActionListener {
     public void draw(Graphics g) {
         for (int i = 0; i < SCREEN_HEIGHT / UNIT_SIZE; i++) {
             g.drawLine(i * UNIT_SIZE, 0, i * UNIT_SIZE, SCREEN_HEIGHT);
+            g.drawLine(0, i * UNIT_SIZE, SCREEN_WIDTH, i * UNIT_SIZE);
         }
     }
 
