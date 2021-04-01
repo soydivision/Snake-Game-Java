b4b7d81410a0712ca5e9abcf627cdbc1be98de20
Sadvakassov Bagzhan <sadvakasov.bagjan@gmail.com>
2021-04-01 16:57:12 +0600

    snake head is there

--- src/main/java/passmesomesugar/com/github/GamePanel.java
+++ src/main/java/passmesomesugar/com/github/GamePanel.java
@@ -54,6 +54,15 @@ public class GamePanel extends JPanel implements ActionListener {
         }
         g.setColor(Color.RED);
         g.fillOval(appleX, appleY, UNIT_SIZE, UNIT_SIZE);
+        for (int i = 0; i < bodyParts; i++) {
+            if (i == 0) {
+                g.setColor(Color.GREEN);
+                g.fillRect(x[i], y[i], UNIT_SIZE, UNIT_SIZE);
+            } else {
+                g.setColor(new Color(45, 180, 0));
+                g.fillRect(x[i], y[i], UNIT_SIZE, UNIT_SIZE);
+            }
+        }
     }
 
     public void newApple() {
@@ -62,6 +71,28 @@ public class GamePanel extends JPanel implements ActionListener {
     }
 
     public void move() {
+//        h left
+//        l right
+//        j down
+//        k up
+        for (int i = bodyParts; i > 0; i--) {
+            x[i] = x[i - 1];
+            y[i] = y[i - 1];
+        }
+        switch (direction) {
+            case 'U':
+                y[0] = y[0] - UNIT_SIZE;
+                break;
+            case 'D':
+                y[0] = y[0] + UNIT_SIZE;
+                break;
+            case 'L':
+                x[0] = x[0] - UNIT_SIZE;
+                break;
+            case 'R':
+                x[0] = x[0] + UNIT_SIZE;
+                break;
+        }
     }
 
     public void checkApple() {
