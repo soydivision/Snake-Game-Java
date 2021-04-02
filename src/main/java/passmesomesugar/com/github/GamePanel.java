e1778a40ad4833229d2bdda33b58ad3876e887c5
Sadvakassov Bagzhan <sadvakasov.bagjan@gmail.com>
2021-04-02 15:45:56 +0600

    snake stops at the border

--- src/main/java/passmesomesugar/com/github/GamePanel.java
+++ src/main/java/passmesomesugar/com/github/GamePanel.java
@@ -99,6 +99,29 @@ public class GamePanel extends JPanel implements ActionListener {
     }
 
     public void checkCollisions() {
+        //checks if head collides with body
+        for (int i = bodyParts; i > 0; i--) {
+            if
+            ((x[0] == x[i]) && (y[0] == y[i])) {
+                running = false;
+            }
+        }
+        //checks if head touches left border
+        if (x[0] < 0) {
+            running = false;
+        }
+        if (x[0] > SCREEN_WIDTH) {
+            running = false;
+        }
+        if (y[0] < 0) {
+            running = false;
+        }
+        if (y[0] > SCREEN_HEIGHT) {
+            running = false;
+        }
+        if (!running) {
+            timer.stop();
+        }
     }
 
     public void gameOver(Graphics g) {
